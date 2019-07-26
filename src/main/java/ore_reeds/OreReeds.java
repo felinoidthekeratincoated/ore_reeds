207e7657dc328376d1c302065e7c1f491e292ba5
Tiffany Jager <tjager@caltech.edu>
2019-07-26 15:44:46 -0400

    changed version number

--- src/main/java/ore_reeds/OreReeds.java
+++ src/main/java/ore_reeds/OreReeds.java
@@ -15,7 +15,7 @@ public class OreReeds
 {
 	public static final String NAME = "Ore Reeds";
 	public static final String MODID = "ore_reeds";
-	public static final String VERSION = "1.12.2-1.0.3";
+	public static final String VERSION = "1.12.2-1.0.5";
 	
 	@Mod.Instance("ore_reeds")
 	public static OreReeds instance;
