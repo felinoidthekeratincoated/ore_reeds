bcbd060323fda30be44061e0999b737f6230d387
Tiffany Jager <tjager@caltech.edu>
2019-08-16 15:04:13 -0400

    nerf thaumcraft aspects on reeds

--- src/main/java/ore_reeds/OreReeds.java
+++ src/main/java/ore_reeds/OreReeds.java
@@ -1,6 +1,8 @@
 package felinoid.ore_reeds;
 
+import felinoid.ore_reeds.integration.thaumcraft.ThaumcraftAspects;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -15,7 +17,9 @@ public class OreReeds
 {
 	public static final String NAME = "Ore Reeds";
 	public static final String MODID = "ore_reeds";
-	public static final String VERSION = "1.12.2-1.0.5";
+	public static final String VERSION = "1.12.2-1.0.6";
+
+	public static boolean thaumcraftLoaded = false;
 	
 	@Mod.Instance("ore_reeds")
 	public static OreReeds instance;
@@ -23,11 +27,16 @@ public class OreReeds
 	@Mod.EventHandler
 	public void preInit(final FMLPreInitializationEvent event) 
 	{
+        thaumcraftLoaded = Loader.isModLoaded("thaumcraft");
 		MinecraftForge.EVENT_BUS.register(ModBlocks.class);
 	}
 	
 	@Mod.EventHandler
-	public void init(final FMLInitializationEvent event) {}
+	public void init(final FMLInitializationEvent event) {
+        if(OreReeds.thaumcraftLoaded) {
+		    MinecraftForge.EVENT_BUS.register(ThaumcraftAspects.class);
+        }
+    }
 	
 	@Mod.EventHandler
 	public void postInit(final FMLPostInitializationEvent event) {}
