0bade9ced23aa2ac42d9e93ed8e478dddac5c758
Tiffany Jager <tjager@caltech.edu>
2020-04-26 11:28:37 -0400

    Remove Thaumcraft support

--- src/main/java/ore_reeds/OreReeds.java
+++ src/main/java/ore_reeds/OreReeds.java
@@ -1,7 +1,6 @@
 package felinoid.ore_reeds;
 
 import felinoid.ore_reeds.blocks.ModBlocks;
-import felinoid.ore_reeds.integration.thaumcraft.ThaumcraftAspects;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
@@ -18,9 +17,7 @@ public class OreReeds
 {
 	public static final String NAME = "Ore Reeds";
 	public static final String MODID = "ore_reeds";
-	public static final String VERSION = "1.12.2-1.1.1";
-
-	public static boolean thaumcraftLoaded = false;
+	public static final String VERSION = "1.14.4-1.1.1";
 	
 	@Mod.Instance("ore_reeds")
 	public static OreReeds instance;
@@ -28,17 +25,6 @@ public class OreReeds
 	@Mod.EventHandler
 	public void preInit(final FMLPreInitializationEvent event) 
 	{
-        thaumcraftLoaded = Loader.isModLoaded("thaumcraft");
 		MinecraftForge.EVENT_BUS.register(ModBlocks.class);
 	}
-	
-	@Mod.EventHandler
-	public void init(final FMLInitializationEvent event) {
-        if(OreReeds.thaumcraftLoaded) {
-		    MinecraftForge.EVENT_BUS.register(ThaumcraftAspects.class);
-        }
-    }
-	
-	@Mod.EventHandler
-	public void postInit(final FMLPostInitializationEvent event) {}
 }
